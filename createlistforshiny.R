d7dca0c3acf6646ca6419c15e1436c7f8cab5c63
unknown <franckx.laurent@gmail.com>
2015-04-16 18:57:58 +0200

    replaced factors in markov chains by characters

--- createlistforshiny.R
+++ createlistforshiny.R
@@ -12,25 +12,37 @@ CreateCorpusList <- function(corpusnames, strglthlist){
       load(paste("US.", corpus, strgtgth +1, "Markov.RData" , sep= ""))
       if(strgtgth <4 ){
         name_df <- paste("US.",corpus, strgtgth +1, "Markov", sep ="" )
-      } else {
-        name_df <- paste(corpus, strgtgth +1, "Markov", sep ="" )
-        
-      }
+       } else {
+         name_df <- paste(corpus, strgtgth +1, "Markov", sep ="" )
+         
+       }
       searchcorpus <- get(name_df)
+      
+      #reduce size of Markov chain with 4 words
+      #if(strgtgth == 4 ){
+        searchcorpus$ML1 <- searchcorpus$ML1[searchcorpus$ML1$freq > 3  , ]
+        searchcorpus$ML2 <- searchcorpus$ML1[searchcorpus$ML2$freq > 3  , ]
+        searchcorpus$ML3 <- searchcorpus$ML1[searchcorpus$ML3$freq > 3  , ]
+        
+      #}
       
       names(searchcorpus$ML1) <- gsub("to_predict","V3", names(searchcorpus$ML1) )
       names(searchcorpus$ML1) <- gsub("V5","V3", names(searchcorpus$ML1) ) 
       names(searchcorpus$ML1) <- gsub("V6","V3", names(searchcorpus$ML1) ) 
+      searchcorpus$ML1$first <- as.character(searchcorpus$ML1$first )
+      searchcorpus$ML1$V3 <- as.character(searchcorpus$ML1$V3 )
       
       names(searchcorpus$ML2) <- gsub("to_predict","V3", names(searchcorpus$ML2) )
       names(searchcorpus$ML2) <- gsub("V5","V3", names(searchcorpus$ML2))
       names(searchcorpus$ML2) <- gsub("V6","V3", names(searchcorpus$ML2))
-      
+      searchcorpus$ML2$first <- as.character(searchcorpus$ML2$first )
+      searchcorpus$ML2$V3 <- as.character(searchcorpus$ML2$V3)
       
       names(searchcorpus$ML3) <- gsub("to_predict","V3", names(searchcorpus$ML3) )
       names(searchcorpus$ML3) <- gsub("V5","V3", names(searchcorpus$ML3))
       names(searchcorpus$ML3) <- gsub("V6","V3", names(searchcorpus$ML3))
-      
+      searchcorpus$ML3$first <- as.character(searchcorpus$ML3$first )
+      searchcorpus$ML3$V3 <- as.character(searchcorpus$ML3$V3 )
       
       #       cat(names(searchcorpus$ML1), "\n")
       #       cat(names(searchcorpus$ML2), "\n")
@@ -54,7 +66,11 @@ CreateCorpusList <- function(corpusnames, strglthlist){
 }
 
 
-corpuslist <- CreateCorpusList(corpusnames, 1:3)
+corpuslist <- CreateCorpusList(corpusnames, 1:4)
+
+#with complete 4-gram list, the total corpuslist takes about 810 Mb - too much for Shiny (on disk: 40 Mb)
+# when reduced to all 4 grams occuring at least 4 times, 613 Mb
+#by transforming factors in characters, one saves a lot of memory: in memort around 164 Mb; on disk 21 Mb (for min freq of 6)
 
 
 save(corpuslist, file = "D:/coursera/dsc_capstone/app1/data/corpuslist.RData")
--- testing_model.R
+++ testing_model.R
@@ -11,9 +11,10 @@ library(stringdist)
 
 set.seed(123)
 corpuses <- c("news", "twitter", "blogs")
-samplesize <- 500
-accu_mat <- expand.grid(corpuses,corpuses)
-names(accu_mat) <- c("TestCorpus", "TrainCorpus")
+samplesize <- 50
+accu_mat <- expand.grid(corpuses,corpuses, 3:4, seq(0.0,1.0,by=0.2))
+names(accu_mat) <- c("TestCorpus", "TrainCorpus", "i", "k")
+accu_mat <- accu_mat[accu_mat$TestCorpus   != accu_mat$TrainCorpus  , ]
 accu_mat$accu1 <- c(0)
 accu_mat$accu2 <- c(0)
 accu_mat$accu3 <- c(0)
@@ -36,18 +37,48 @@ accu_manum <- accu_mat
 list_of_means <- list()
 
 system.time(
-  for(k in seq(0.1,0.7, by = 0.1)){
-    for(i in 2:4){
+ # for(k in seq(0.1,0.7, by = 0.1)){
+ #testing for lower n-gram does not really make sense     
+    for(i in 4:5){
       #for(i in 5){   
       cat("started for i = ", i , "\n")
       
       #  for(k in seq(0.13,0.17, by = 0.02)){
       #    cat("started for k = ", k , "\n")
       for(TestCorpus in corpuses){
-        for(TrainCorpus in corpuses){
-          DfToSample <- corpuslist[[paste("US.",TestCorpus,i,"Markov",sep="")]]$ML3
-          TextToSampleId <- sample(nrow(DfToSample), samplesize)
-          TextToTest <- DfToSample[TextToSampleId  , ]
+        resdir <- "D:/coursera/caps_res/"
+        if(i < 5){
+          DfToSample <- read.table(paste(resdir, "US.",TestCorpus,"Tokened", i,"Gr_uniq.txt",sep=""), stringsAsFactors = FALSE)
+        } else {
+          DfToSample <- read.table(paste(resdir, TestCorpus,"Tokened", i,"Gr_uniq.txt",sep=""), stringsAsFactors = FALSE)
+        }
+        
+        cat("Started with test set", TestCorpus, ".\n")
+        
+        TextToSampleId <- sample(nrow(DfToSample), samplesize)
+        TextToTest <- DfToSample[TextToSampleId  , ]
+        rm(DfToSample)
+        gc()
+        TextToTest <- TextToTest[, 2]
+        testword_vec <- character(length(TextToTest))
+        tesstring_vec <- character(length(TextToTest))
+        for(j in seq_along(1:length(TextToTest))){
+          splitstring <- strsplit(TextToTest[j], " ")[[1]]
+          stringlenth <- length(splitstring)
+          testword_vec[j] <- splitstring[stringlenth]  
+          firstwords <- splitstring[1]
+          for(r in 2:(stringlenth-1)) firstwords <- paste(firstwords,splitstring[r], sep= " ")         
+          tesstring_vec[j] <- firstwords
+        }
+        tesstring_vec <- rbind(tesstring_vec,testword_vec)
+        TextToTest <- t(rbind(TextToTest,tesstring_vec))  
+        colnames(TextToTest) <- c("TextToTest", "first","V3")
+        TextToTest <- as.data.frame(TextToTest)
+        for(k in seq(0.0,1.0,by=0.2)){
+          
+        for(TrainCorpus in setdiff(corpuses,TestCorpus)){
+          cat("Started with training set", TrainCorpus, ".\n")
+          
           CorpusUsedForTest <- TrainCorpus
           TextToTest$first <- as.character(TextToTest$first)
           
@@ -56,43 +87,57 @@ system.time(
           #  TestResult <- do.call("cbind", TestResult)
           if(nrow(TestResult) == 3) TestResult <- t(TestResult)
           TextToTest <- cbind(TextToTest,TestResult)
-          TextToTest[ , 4] <- as.character(TextToTest[ , 4])
-          TextToTest[ , 5] <- as.character(TextToTest[ , 5])
-          TextToTest[ , 6] <- as.character(TextToTest[ , 6])
+          TestResult[ , 1] <- as.character(TestResult[ , 1])
+          TestResult[ , 2] <- as.character(TestResult[ , 2])
+          TestResult[ , 2] <- as.character(TestResult[ , 3])
+                 
           #    if(any(TextToTest[ , 4] == TextToTest[ , 5]) | any(TextToTest[ , 4] == TextToTest[ , 6])) stop("still duplicate values")
           TextToTest$V3 <- as.character(TextToTest$V3)
-          accuracy1num <- sum(TextToTest$V3 == TextToTest[, 4], na.rm = TRUE)
-          accuracy2num <- sum(TextToTest$V3 == TextToTest[, 5], na.rm = TRUE)
-          accuracy3num <- sum(TextToTest$V3 == TextToTest[, 6], na.rm = TRUE)
+          accuracy1num <- sum(TextToTest$V3 == TestResult[, 1], na.rm = TRUE)
+          accuracy2num <- sum(TextToTest$V3 == TestResult[, 2], na.rm = TRUE)
+          accuracy3num <- sum(TextToTest$V3 == TestResult[, 3], na.rm = TRUE)
           accuracy1 <- accuracy1num/nrow(TextToTest)
           accuracy2 <- (accuracy1num + accuracy2num)/nrow(TextToTest)
           accuracy3 <- (accuracy1num + accuracy2num + accuracy3num)/nrow(TextToTest)
           #           TextToTest_name <- paste("tst_vs_tr",i, TestCorpus,  TrainCorpus, "k",k, sep="_")
           #           assign(TextToTest_name, TextToTest)
           #           save(TextToTest, file = paste(TextToTest_name, "RData", sep="."))
-          #     cat(sprintf("%1.2f"  ,c(accuracy1, accuracy2,accuracy3 ) ), "\n")
-          accu_mat[accu_mat$TestCorpus == TestCorpus & accu_mat$TrainCorpus == TrainCorpus,  c("accu1","accu2","accu3")] <- sprintf("%1.2f"  , c(accuracy1, accuracy2,accuracy3 ))
-          accu_manum[accu_mat$TestCorpus == TestCorpus & accu_mat$TrainCorpus == TrainCorpus,  c("accu1","accu2","accu3")] <- c(accuracy1, accuracy2,accuracy3 )
+          cat(sprintf("%1.2f"  ,c(accuracy1, accuracy2,accuracy3 ) ), "\n")
+          accu_mat[accu_mat$TestCorpus == TestCorpus & accu_mat$TrainCorpus == TrainCorpus & accu_mat$i == i &   accu_mat$k == k,  c("accu1","accu2","accu3")] <- sprintf("%1.2f"  , c(accuracy1, accuracy2,accuracy3 ))
+          accu_manum[accu_mat$TestCorpus == TestCorpus & accu_mat$TrainCorpus == TrainCorpus & accu_mat$i == i &   accu_mat$k == k,  c("accu1","accu2","accu3")] <- c(accuracy1, accuracy2,accuracy3 )
           
           
         }
+        cat("Finalised for prediction based on discount factor", k , "and testcorpus", TestCorpus, ".\n")
+        
+        
       }
       
+      }
     
-    name_accumat <- paste("accum_mat_",i, "gr_amatch", "k", k, sep="")
-    assign(name_accumat, accu_mat)
-    save(accu_mat, file = paste(name_accumat, "RData", sep="."))
-    df_for_colmeans <- accu_manum[accu_manum$TestCorpus !=  accu_manum$TrainCorpus , c("accu1","accu2","accu3")]
-    list_of_means[[ paste("ngr",i, "gr_amatch", "k", k, sep="_") ]] <- colMeans(df_for_colmeans, na.rm = FALSE, dims = 1)
+#     df_for_colmeans <- accu_manum[accu_manum$TestCorpus !=  accu_manum$TrainCorpus , c("accu1","accu2","accu3")]
+#     list_of_means[[ paste("ngr",i, "gr_amatch", "k", k, sep="_") ]] <- colMeans(df_for_colmeans, na.rm = FALSE, dims = 1)
     cat("Finalised for prediction based on ", i , " words.\n")
     }
  
-  cat("Finalised for prediction based on discount factor", k , ".\n")
-  }
+   
 )
 
 
-df_of_means <- do.call("rbind", list_of_means)
+#name_accumat <- paste("accum_mat_",i, "gr_amatch", "k", k, sep="")
+name_accumat <- "accuracy_test"
+assign(name_accumat, accu_mat)
+save(accu_mat, file = paste(name_accumat, "RData", sep="."))
+
+
+
+#df_of_means3 <- do.call("rbind", list_of_means)
 #save(accu_mat, file = "accum_mat_2gr.RData")
-save(df_of_means, file = "mean_accuracy.RData")  
+#save(df_of_means_new, file = "mean_accuracy.RData")  
+
+#save(df_of_means3, file = "mean_accuracy1.RData")  
+
+# df_of_means <- df_of_means2
+# save(df_of_means, file = "mean_accuracy.RData")  
+
   
